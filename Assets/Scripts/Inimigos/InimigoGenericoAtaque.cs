7993fe80921c02ff76d6bd7579f477510d1523ba
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2025-09-17 18:07:12 -0300

    minotauro

--- Assets/Scripts/Inimigos/InimigoGenericoAtaque.cs
+++ Assets/Scripts/Inimigos/InimigoGenericoAtaque.cs
@@ -37,7 +37,7 @@ public class InimigoGenericoAtaque : InimigoEstado
 
     private void IniciarAtaque()
     {
-        animator.SetTrigger("AtaqueComEspada");
+        animator.SetTrigger("Atacar");
     }
 
     public void RealizarAtaque()
