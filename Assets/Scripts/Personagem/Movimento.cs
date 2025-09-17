f44d0f6839eb74d9481d79296936b8a2f3d6fa54
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2025-09-17 11:02:46 -0300

    Movimento Personagem e criacao dos clips de animacao

---
+++ Assets/Scripts/Personagem/Movimento.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float entradaHorizontal;
+    [SerializeField] private float velocidade = 5f;
+
+    private bool estaNoChao;
+    [SerializeField] private Transform pePersonagem;
+    [SerializeField] private LayerMask cenarioLayer;
+
+    private bool saltoExtra;
+
+    private DirecaoPersonagem direcaoAtual;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direcaoAtual = DirecaoPersonagem.DIREITA;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        entradaHorizontal = Input.GetAxis("Horizontal");
+
+        estaNoChao = Physics2D.OverlapCircle(pePersonagem.position,0.3f,cenarioLayer);
+
+        if (estaNoChao)
+        {
+            saltoExtra = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (estaNoChao)
+            {
+                Saltar();
+            }
+            else if (saltoExtra)
+            {
+                saltoExtra = false;
+                Saltar();
+            }
+        }
+
+        if (entradaHorizontal > 0)
+        {
+            GirarPersonagem(DirecaoPersonagem.DIREITA);
+        }else if(entradaHorizontal < 0)
+        {
+            GirarPersonagem(DirecaoPersonagem.ESQUERDA);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(entradaHorizontal * velocidade, rb.linearVelocity.y);
+    }
+
+    private void Saltar()
+    {
+        rb.linearVelocity = Vector2.zero;
+        rb.AddForce(Vector2.up * 300);
+    }
+
+    private void GirarPersonagem(DirecaoPersonagem direcao)
+    {
+        if (direcao == direcaoAtual)
+        {
+            return;
+        }
+
+        direcaoAtual = direcao; 
+        if(direcaoAtual == DirecaoPersonagem.DIREITA)
+        {
+            transform.eulerAngles = new Vector3 (0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+}
+
+enum DirecaoPersonagem { ESQUERDA,DIREITA}
